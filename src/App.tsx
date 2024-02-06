dbb6ee285063d8ce01608cfdb69231c51f483a8c
AYUSH RANGHAR <ayushranghar@AYUSHs-MacBook-Air.local>
2024-02-06 22:49:05 +0530

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { Route, Routes } from "react-router-dom";
+import "./global.css";
+import SigninForm from "./_auth/forms/SigninForm";
+import { Home } from "./root/pages";
+import SignupForm from "./_auth/forms/SignupForm";
+import AuthLayout from "./_auth/forms/AuthLayout";
+import RootLayout from "./root/RootLayout";
+
+const App = () => {
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/* public routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
+
+        {/* private routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
+};
+
+export default App;
---
+++ src/_auth/forms/SignupForm.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/ui/button'
+
+const SignupForm = () => {
+  return (
+    <div>
+        <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default SignupForm
