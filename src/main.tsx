04d84bc0b37b2da881c3c3e201042b2cf60df0ef
AYUSH RANGHAR <ayushranghar@AYUSHs-MacBook-Air.local>
2024-02-12 00:42:41 +0530

    currency converter added

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 // import App from "./App.tsx";
 // import BgChanger from "./BgChanger";
-import RandomTextGenerator from "./components/RandomTextGenerator";
+// import RandomTextGenerator from "./components/RandomTextGenerator";
+import Currency from "./currency/Currency";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RandomTextGenerator />
+    <Currency />
   </React.StrictMode>
 );
